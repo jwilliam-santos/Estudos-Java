81246c147794e1251bb8d3600fe534c322f2b90d
Jorge William <jorgew965@gmail.com>
2026-06-24 17:28:15 -0300

    exercicio 6 feito
    completo dia 24/06/2026

--- Cap03/ex6.java
+++ Cap03/ex6.java
@@ -1,2 +1,32 @@
 //Crie um array de 10 inteiros. 
 //Preencha todas as posições com o valor 10. Depois, use um laço para dobrar o valor de cada posição (valor * 2) e imprima o resultado final.
+public class ex6{
+    public static void main(String[]args){
+        int[] array = new int[10];
+        array[0] = 10;
+        array[1] = 10;
+        array[2] = 10;
+        array[3] = 10;
+        array[4] = 10;
+        array[5] = 10;
+        array[6] = 10;
+        array[7] = 10;
+        array[8] = 10;
+        array[9] = 10;
+        int i = 0;
+        while(i<9){
+            System.out.println(array[i]*2);
+            i++;
+        };
+
+
+
+
+
+
+
+
+
+    }
+
+}
