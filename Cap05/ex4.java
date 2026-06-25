f3db4000157c3dcefe45877528b0d746d68391a4
Jorge William <jorgew965@gmail.com>
2026-06-25 00:21:30 -0300

    exercicio 4 feito
    completo dia 25/06/2026

---
+++ Cap05/ex4.java
@@ -0,0 +1,10 @@
+//Faça um programa que percorra os números de 1 a 20 e imprima apenas os valores que forem pares.
+
+public class ex4{
+    public static void main (String[]args){
+        for( int i=0;i<=20;i++){
+            System.out.println(i);
+
+        }
+    }
+}
