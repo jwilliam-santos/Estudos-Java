6b729e516302933aabe9d9587549fee5a0c0f134
Jorge William <jorgew965@gmail.com>
2026-06-25 00:19:40 -0300

    exercicio 1 feito
    completo dia 25/06/2026

---
+++ Cap05/ex1.java
@@ -0,0 +1,13 @@
+//Escreva um programa que faça uma contagem regressiva de 10 até 1 utilizando um loop for e, no final, exiba a mensagem "Decolar!".
+
+
+
+public class ex1{
+    public static void main(String[]args){
+        for( int i=11;i>=1;i--){
+            System.out.println(i);
+        }
+        System.out.println("Decolar");
+    }
+    
+}
