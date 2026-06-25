a2d98e82f56aa28ce8349e49eea7eb10d5adf5df
Jorge William <jorgew965@gmail.com>
2026-06-25 00:27:29 -0300

    exercicio 2 feito
    completo dia 25/06/2026

---
+++ Cap05/ex2.java
@@ -0,0 +1,9 @@
+//Crie um programa que possua uma variável int numero fixa e mostre a tabuada desse número do 1 ao 10 no console.
+public class ex2 {
+    public static void main(String[]args){
+        int a = 7;
+        for (int i = 0; i <= 10; i++) {
+          System.out.println(a*i);  
+        }
+    }
+}
