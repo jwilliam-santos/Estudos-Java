492df39064475c9b72aadb6b013a1b59bc540618
Jorge William <jorgew965@gmail.com>
2026-06-28 23:47:13 -0300

    exercicio 1 feito
    completo dia 28/06/2026,
    Vortei

--- Cap06/ex1.java
+++ Cap06/ex1.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+//Crie um ArrayList de números inteiros (Integer), adicione 5 valores manualmente usando .add() e exiba a soma de todos os elementos na tela utilizando um laço for-each.
 public class ex1 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> inteiros = new ArrayList<>();
+        inteiros.add(1);
+        inteiros.add(2);
+        inteiros.add(3);
+        inteiros.add(4);
+        inteiros.add(5);
+        for( int s:inteiros){
+            System.out.println(s);
+        }
+    }
 }
