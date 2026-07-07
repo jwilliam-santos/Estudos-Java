1a670ecc57e10663a61e66f19da5705656be984f
jwilliam-santos <jorgew965@gmail.com>
2026-07-07 02:24:16 -0300

    ex4 feito
    completo dia 7/7/2026

---
+++ Cap09/ex4.java
@@ -0,0 +1,18 @@
+/*Escreva um método chamado gerarDescarte.
+Dentro dele, seu objetivo é dar vida a um novo objeto Jogador de forma que
+ele seja jogado na fila do Garbage Collector no mesmo instante e na mesma linha em que foi criado.
+Nenhuma variável de referência pode tocar ou segurar esse objeto. */
+class b{
+    public void gerarDescarte(){
+        String a;
+    }
+}
+
+
+public class ex4 {
+    public static void main(String[] args) {
+        b B = new b();
+        B.gerarDescarte();
+    }
+    
+}
