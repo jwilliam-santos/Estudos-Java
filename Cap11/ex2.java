d4551b55ee2a717a32ab97aef3be123386e81dd2
jwilliam-santos <jorgew965@gmail.com>
2026-07-09 16:31:10 -0300

    ex 2 feito
    completo dia 9/7/2026

---
+++ Cap11/ex2.java
@@ -0,0 +1,11 @@
+//Escreva um programa em Java para adicionar o elemento especificado ao final de um conjunto hash.
+import java.util.HashSet;
+public class ex2 {
+    public static void main(String[] args) {
+        HashSet<String> a = new HashSet<>();
+        a.add("Algo");
+        a.add("Sla");
+        a.add("Coisa");
+        a.add("Computador");
+    }
+}
