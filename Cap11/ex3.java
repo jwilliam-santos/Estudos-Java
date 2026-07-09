2460e6d5a70b2f4679cb5608a40f89b7bd54a1ca
jwilliam-santos <jorgew965@gmail.com>
2026-07-09 16:31:25 -0300

    ex3 feito
    completo dia 9/7/2026

---
+++ Cap11/ex3.java
@@ -0,0 +1,9 @@
+//Escreva um programa em Java para associar o valor especificado à chave especificada em um HashMap.
+import java.util.HashMap;
+public class ex3 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> a = new HashMap<>();
+        a.put("Algo",12 );
+        System.out.println(a);
+ }
+}
