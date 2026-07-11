46cf65ac3ab280f544b992a27ce21a1a2f809426
jwilliam-santos <jorgew965@gmail.com>
2026-07-10 21:34:44 -0300

    Fim cap 12
    Por algum motivo quase ñ tem exercicios so de lambda no w3resouce

---
+++ Cap12/ex3.java
@@ -0,0 +1,6 @@
+//Escreva um programa em Java para implementar uma expressão lambda que converta uma lista de strings em maiúsculas e minúsculas.
+
+
+public class ex3 {
+    
+}
---
+++ Cap12/ex4.java
@@ -0,0 +1,6 @@
+//Escreva um programa em Java para implementar uma expressão lambda que encontre os valores máximo e mínimo em uma lista de números inteiros.
+
+
+public class ex4 {
+    
+}
