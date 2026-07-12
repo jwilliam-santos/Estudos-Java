ba1250e536e52a0a66d39f8304d60feb1dee4aec
jwilliam-santos <jorgew965@gmail.com>
2026-07-12 19:55:36 -0300

    ex5 feito
    completo dia 5 de julho de 2026

---
+++ Java© como programar 3ed/cap2/ex5.java
@@ -0,0 +1,12 @@
+/*Escreva um aplicativo que leia cinco inteiros, além de determinar e imprimir o maior e o menor inteiro
+no grupo. Utilize somente as técnicas de programação que você aprendeu neste capítulo. */
+public class ex5 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        if (e>d && e> c && e>b && e>a){System.out.println("E e maior");}
+    }
+}
