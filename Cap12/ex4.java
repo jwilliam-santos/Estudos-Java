38bdf90a0320763039198ed7b2de13d68904fc8f
jwilliam-santos <jorgew965@gmail.com>
2026-07-12 14:56:36 -0300

    UPDATE

--- Cap12/ex4.java
+++ Cap12/ex4.java
@@ -1,6 +1,15 @@
 //Escreva um programa em Java para implementar uma expressão lambda que encontre os valores máximo e mínimo em uma lista de números inteiros.
-
+import java.util.ArrayList;
 
 public class ex4 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.stream();
+
+    }    
 }
