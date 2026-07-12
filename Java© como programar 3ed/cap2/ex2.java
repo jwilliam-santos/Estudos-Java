568af12036c5c3697e609df3b02064db756f9af2
jwilliam-santos <jorgew965@gmail.com>
2026-07-12 17:34:24 -0300

    ex2 feito
    completo dia 12/7/26

---
+++ Java© como programar 3ed/cap2/ex2.java
@@ -0,0 +1,15 @@
+/*Escreva um aplicativo que solicite ao usuário inserir dois inteiros, obtenha dele esses números e imprima sua soma, produto,
+diferença e quociente (divisão). Utilize as técnicas mostradas na Figura 2.7.*/
+import java.util.Scanner;
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("Digite o Primeiro Inteiro");
+        int b = a.nextInt();
+        System.out.print("Digite o segundo inteiro");
+        int c =a.nextInt();
+        int diferença  = b-c;
+        System.out.print("soma é " + b+c + "produto é " + b*c + "diferença " + diferença  + " divisão " + b/c);
+
+    }    
+}
