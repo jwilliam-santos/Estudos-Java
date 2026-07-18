7ace2b86fedf5b0d80344e24055f1a1b04b3336c
jwilliam-santos <jorgew965@gmail.com>
2026-07-18 02:25:29 -0300

    ex1 feito
    completo dia 18/7/26
    fiz so 1 pq eu ja vi esse assunto no (use a cabeça java)

---
+++ Java© como programar 3ed/cap5/ex1.java
@@ -0,0 +1,11 @@
+/*Escreva um aplicativo que calcula o produto dos números inteiros ímpares de 1 a 15. */
+public class ex1 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 15;i++){
+            if (i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
